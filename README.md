09745ea525a68855a78ce80035f4511a9040f3c8
qinfanpeng <qinfanpeng@gmail.com>
2013-06-27 16:20:02 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-pdf_renderer
-============
+PdfRenderer
+===========
+
+This is a extention fo rails controller render
+
+### Usage
+    class HomeController < ApplicationController
+      def index
+        respond_to do |format|
+          format.pdf { render :pdf => "contents" }
+        end
+      end
+    end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '72bae700f9b511f5b04d39bc618b33b957490774308f81c5ccb8a2f83b056961340b795363016fd99bdc14707ae599471c1a2bdcfd38c33944e08d491025e1b5'
